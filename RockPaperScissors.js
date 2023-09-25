76ff38a666b56cde312d74b78e2c6cdd16987dcd
Seb1123 <sebastianierquiaga@gmail.com>
2023-09-25 03:15:41 -0400

    Made a UI for the Rock Paper Scissors game.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -24,56 +24,123 @@ function getPlayerChoice() {
     alert("Invalid input. Please try again.")
 }
 
-function RPS(playerSelection, computerSelection) {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    b = playerSelection;
-    a = computerSelection;
-    console.log("The computer chose... " + a + "!");
-    let x;
+const compmess = document.querySelector('div#computer-message');
+const message = document.querySelector('div#game-message');
+const result = document.querySelector('div#result-message');
 
+let player = 0;
+let comp = 0;
+
+function RPS(z) {
+    b = z;   
+    a = getComputerChoice();
+    compmess.textContent = ("\n The computer chose... " + a + "! \n\n");
+    let x;
+    
     if (b == 'Rock' && a == 'Scissor'){
         x = 'win';
-        return ("You " + x + "! " + b + " beats " + a + ".")
+        message.textContent = ("You " + x + "! " + b + " beats " + a + ".");
+        player += 1;
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
+        if (player == 5) {
+            player = 0
+            comp = 0
+            result.textContent = ("You beat the computer, congratulations!")
+        }
     } else if (b == 'Paper' && a == 'Rock') {
         x = 'win';
-        return ("You " + x + "! " + b + " beats " + a + ".")
+        message.textContent = ("You " + x + "! " + b + " beats " + a + ".");
+        player += 1;
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
+        if (player == 5) {
+            player = 0
+            comp = 0
+            result.textContent = ("You beat the computer, congratulations!")
+        }
     }  else if (b == 'Scissor' && a == 'Paper') {
         x = 'win';
-        return ("You " + x + "! " + b + " beats " + a + ".")
+        message.textContent = ("You " + x + "! " + b + " beats " + a + ".");
+        player += 1;
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
+        if (player == 5) {
+            player = 0
+            comp = 0
+            result.textContent = ("You beat the computer, congratulations!")
+        }
+        
     }
     
     else if (b == 'Rock' && a == 'Paper'){
-        x = 'lose'; 
-        return ("You " + x + "! " + a + " beats " + b + ".")
+        x = 'lose';
+        message.textContent = ("You " + x + "! " + a + " beats " + b + ".");
+        comp += 1;
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
+        if (comp == 5) {
+            player = 0
+            comp = 0
+            result.textContent = ("The computer won, try again!")
+        }
     } else if (b == 'Paper' && a == 'Scissor'){
-        x = 'lose'; 
-        return ("You " + x + "! " + a + " beats " + b + ".")
+        x = 'lose';
+        message.textContent = ("You " + x + "! " + a + " beats " + b + ".");
+        comp += 1;
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
+        if (comp == 5) {
+            player = 0
+            comp = 0
+            result.textContent = ("The computer won, try again!")
+        }
     } else if (b == 'Scissor' && a =='Rock') {
-        x = 'lose'; 
-        return ("You " + x + "! " + a + " beats " + b + ".")
+        x = 'lose';
+        message.textContent = ("You " + x + "! " + a + " beats " + b + ".");
+        comp += 1;
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
+        if (comp == 5) {
+            player = 0
+            comp = 0
+            result.textContent = ("The computer won, try again!")
+        }
     }
 
     else if (b == 'Paper' && a == 'Paper') {
         x = 'tie';
-        return ("It's a " + x + "!")
+        message.textContent = ("It's a " + x + "! No winners...");
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
     } else if (b == 'Scissor' && a == 'Scissor') {
         x = 'tie';
-        return ("It's a " + x + "!")
+        message.textContent = ("It's a " + x + "! No winners...");
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
     } else if (b == 'Rock' && a == 'Rock') {
         x = 'tie';
-        return ("It's a " + x + "!")
+        message.textContent = ("It's a " + x + "! No winners...");
+        result.textContent = ("Player: " + player + "\nComp: " + comp);
     }
-
-    alert("Invalid input. Please try again.");
 }
 
+const container = document.querySelector('.container');
+
+const b1 = document.querySelector('button#rock');
+b1.addEventListener('click', () => RPS('Rock'));
+
+const b2 = document.querySelector('button#scissor');
+b2.addEventListener('click', () => RPS('Scissor'));
+
+const b3 = document.querySelector('button#paper');
+b3.addEventListener('click', () => RPS('Paper'));
+
+
+
+/*
+buttons = Array.from(document.querySelectorAll('id'));
+buttons.forEach(app());
+*/
+
+/*
 function game(){
     Player = 0;
     Computer = 0;
 
     g1 = RPS();
-    /* Game 1*/
     if (g1.includes('win')) {
         Player += 1;
         console.log(g1);
@@ -87,7 +154,6 @@ function game(){
     }
 
     g2 = RPS();
-    /* Game 2*/
     if (g2.includes('win')) {
         Player += 1;
         console.log(g2);
@@ -101,7 +167,6 @@ function game(){
     }
     
     g3 = RPS();
-    /* Game 3*/
     if (g3.includes('win')) {
         Player += 1;
         console.log(g3);
@@ -115,7 +180,6 @@ function game(){
     }
 
     g4 = RPS();
-    /* Game 4*/
     if (g4.includes('win')) {
         Player += 1;
         console.log(g4);
@@ -129,7 +193,6 @@ function game(){
     }
 
     g5 = RPS();
-    /* Game 5*/
     if (g5.includes('win')) {
         Player += 1;
         console.log(g5);
@@ -142,7 +205,6 @@ function game(){
         console.log(g5);
     }
 
-    /* Final Outcome */
     if (Player > Computer) {
         return ("You had the most wins (" + Player + ") out of five rounds! Congratulations, you beat the computer!")
     } else if (Player < Computer) {
@@ -150,5 +212,8 @@ function game(){
     }
     return ("You have the same score as the computer. This is a tie!")
 }
+*/
 
+/*
 console.log(game());
+*/
